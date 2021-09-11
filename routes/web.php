14dab314cda6ea1320c905b2d74fc74a4e301345
Kalema Edgar <eked290@gmail.com>
2021-09-11 22:27:33 +0300

    Add caching for the blogs

--- routes/web.php
+++ routes/web.php
@@ -18,16 +18,19 @@ Route::get('/', function () {
 });
 
 Route::get('/posts/{post}', function ($slug) {
-    $path = __DIR__ . "/../resources/posts/$slug.html";
-
-    if (!file_exists($path)) {
-        // ddd('File does not exist');
+    if (!file_exists($path = __DIR__ . "/../resources/posts/$slug.html")) {
         return redirect('/');
     }
 
-    $post = file_get_contents($path);
-    return view('post', [
-        'post' => $post
-    ]);
+    // // $post = cache()->remember("posts.{$slug}", 5, function () use ($path) {
+    // $post = cache()->remember("posts.{$slug}", now()->addSeconds(5), function () use ($path) {
+    //     // var_dump('file_get_contents');
+    //     return file_get_contents($path);
+    // });
+
+    // Short form of the above. Cache the page for 5 seconds
+    $post = cache()->remember("posts.{$slug}", 5, fn () => file_get_contents($path));
+
+    return view('post', ['post' => $post]);
 })->where('post', '[A-z_\-]+'); // Regex to check that the slug follows a pattern (capital and small letters, underscore, dash)
 // })->whereAlpha('post'); // Incase the slug should only contain alphabets (capital and small letters)
