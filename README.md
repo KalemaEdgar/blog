a8b043e8bcc8b96e6f3db55caf4065894c46f679
Kalema Edgar <eked290@gmail.com>
2021-09-11 21:29:54 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# blog
+ Blog - Testing Laravel 8 and PHP PHP 8.0.10
---
+++ resources/views/posts.blade.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+
+<title>My Blog</title>
+<link rel="stylesheet" href="/app.css">
+
+<body>
+    <article>
+        <h1><a href="/posts/my-first-post">My first post</a></h1>
+        <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum asperiores maiores officia fugiat obcaecati,
+            quam rerum eligendi blanditiis non consequatur ex soluta numquam magnam aspernatur, est suscipit!
+            Perferendis suscipit vel labore voluptas ipsa officiis inventore illo ab quae, laboriosam magni voluptatum
+            ipsam iusto! Architecto rerum sit odio minima iste earum ipsa, totam doloremque cupiditate amet quaerat
+            soluta! Inventore delectus reiciendis quis error vero neque quidem autem ipsum odit rerum, perferendis illo
+            repudiandae fuga voluptatem vitae ad omnis alias, sunt architecto magni aut sit. Rem eos labore neque.
+            Asperiores explicabo fuga quidem modi sint totam dolorem? Nam libero eum numquam quasi?
+        </p>
+    </article>
+
+    <article>
+        <h1><a href="/posts/my-second-post">My second post</a></h1>
+        <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Accusantium consequuntur perspiciatis officia
+            quibusdam, expedita, quae magnam delectus, ex illo suscipit natus non ad est. Illo rerum ex vero cumque
+            dolorem reprehenderit ipsum placeat veritatis quibusdam. Itaque necessitatibus enim aspernatur vel
+            recusandae perspiciatis ullam quis laborum in impedit ducimus, vitae illum dicta repellat molestias, veniam
+            assumenda reiciendis fuga ad voluptatibus nisi. Sint minima ipsa corrupti nihil laboriosam quam delectus
+            ipsum praesentium, nulla dolorum optio deleniti, natus officia illum cumque cupiditate culpa assumenda at
+            quae animi. Modi alias quam animi? Inventore est laborum esse nostrum quam.
+        </p>
+    </article>
+</body>
