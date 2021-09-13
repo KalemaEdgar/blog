2ff580c186a204151d77911308f2ce8a9b5276f3
Kalema Edgar <eked290@gmail.com>
2021-09-14 00:11:20 +0300

    Adding templating using blade

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -1,20 +1,13 @@
-<!DOCTYPE html>
+@extends('layout')
 
-<title>My Blog</title>
-<link rel="stylesheet" href="/app.css">
-
-<body>
+@section('content')
     <article>
-        <h1>
-            {{ $post->title }}
-        </h1>
+        <h1>{{ $post->title }}</h1>
 
         <div>
-            <!-- Use this if you dont want to escape the text.
-            Take caution cause Laravel wont escape or handle this if it has issues -->
             {!! $post->body !!}
         </div>
     </article>
 
     <a href="/">Go back</a>
-</body>
+@endsection
