ae25abdb6f0c1f01965a5833096a3d9a4fa712b0
Kalema Edgar <eked290@gmail.com>
2021-09-14 23:15:07 +0300

    Dealing with Mass Assignment

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -1,5 +1,4 @@
 <?php
-
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
@@ -8,4 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['title', 'excerpt', 'body']; // Only allow these to be mass-assginable. Anything out of this uses the default values even specified in the create statement
+    // protected $guarded = ['id']; // Allow everything to be assigned other than the id. Even if you have the id in the query, the default value is used as specified in the table
 }
