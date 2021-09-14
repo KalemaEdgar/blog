f49a531c94c5c2da7f2715de1d6fa2d5b339ba23
Kalema Edgar <eked290@gmail.com>
2021-09-14 23:32:35 +0300

    Another option for route key binding

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -10,4 +10,11 @@ class Post extends Model
 
     protected $fillable = ['title', 'excerpt', 'body']; // Only allow these to be mass-assginable. Anything out of this uses the default values even specified in the create statement
     // protected $guarded = ['id']; // Allow everything to be assigned other than the id. Even if you have the id in the query, the default value is used as specified in the table
+
+    // Use this for the route to search by the slug instead of the default id
+    // Also this is used if you dont specify the key in the route ({post:slug})
+    public function getRouteKeyName()
+    {
+        return 'slug';
+    }
 }
