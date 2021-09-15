82e4bfebb6179aaa5c94432029870672df69c972
Kalema Edgar <eked290@gmail.com>
2021-09-15 11:43:38 +0300

    Added Categories and relationships

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -8,13 +8,19 @@ class Post extends Model
 {
     use HasFactory;
 
-    protected $fillable = ['title', 'excerpt', 'body']; // Only allow these to be mass-assginable. Anything out of this uses the default values even specified in the create statement
+    protected $fillable = ['title', 'excerpt', 'body', 'slug', 'category_id']; // Only allow these to be mass-assginable. Anything out of this uses the default values even specified in the create statement
     // protected $guarded = ['id']; // Allow everything to be assigned other than the id. Even if you have the id in the query, the default value is used as specified in the table
 
     // Use this for the route to search by the slug instead of the default id
     // Also this is used if you dont specify the key in the route ({post:slug})
-    public function getRouteKeyName()
+    // public function getRouteKeyName()
+    // {
+    //     return 'slug';
+    // }
+
+    // Relationships
+    public function category()
     {
-        return 'slug';
+        return $this->belongsTo(Category::class);
     }
 }
