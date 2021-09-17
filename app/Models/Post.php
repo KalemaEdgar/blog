4f8f3b3d661de3856a273c7454bdfa8d77b04bf9
Kalema Edgar <eked290@gmail.com>
2021-09-17 21:56:38 +0300

    Filter by authors

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -36,6 +36,14 @@ class Post extends Model
                 fn ($query) => $query->where('slug', $category)
             )
         );
+
+        $query->when(
+            $filters['author'] ?? false,
+            fn ($query, $author) => $query->whereHas(
+                'author',
+                fn ($query) => $query->where('username', $author)
+            )
+        );
     }
 
     // Relationships
