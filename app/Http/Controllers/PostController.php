fe72e6abb9ff46f8e840a5d5afbfdd1bcec48793
Kalema Edgar <eked290@gmail.com>
2021-09-17 20:52:50 +0300

    Namespace views according to controllers

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -8,15 +8,15 @@ class PostController extends Controller
     public function index()
     {
         // Because of the scope query filter, we are able to retrieve all posts and specific criteria all in the same method
-        return view('posts', [
+        return view('posts.index', [
             'posts' => Post::latest()->filter(request(['search', 'category']))->get(),
         ]);
     }
 
     public function show(Post $post)
     {
-        // Find a post by its slug and pass it to a view called "post"
-        return view('post', [
+        // Find a post by its slug and pass it to a view "posts/show.blade.php"
+        return view('posts.show', [
             'post' => $post
         ]);
     }
