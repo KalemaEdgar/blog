93e0dd7d12999b61e00698114dc17a36c3c90767
Kalema Edgar <eked290@gmail.com>
2021-09-18 15:11:38 +0300

    Handle the unique username validation exception

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -15,7 +15,7 @@ class RegisterController extends Controller
         // If the request validation fails, Laravel will automatically redirect you back to the page
         $attributes = request()->validate([
             'name' => 'required|max:255',
-            'username' => 'required|min:3|max:255',
+            'username' => 'required|min:3|max:255|unique:users,username',
             'email' => 'required|email|max:255',
             'password' => 'required|min:8|max:255',
         ]);
