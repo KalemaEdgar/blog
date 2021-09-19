376e3215e7656b917dc1cd7d6621c153ae1f7c96
Kalema Edgar <eked290@gmail.com>
2021-09-19 23:55:09 +0300

    Amend the redirection message after login failure

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -2,6 +2,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Validation\ValidationException;
 
 class SessionsController extends Controller
 {
@@ -13,6 +14,7 @@ class SessionsController extends Controller
     public function store()
     {
         // Validate the request
+        // If the validation fails, Laravel automatically redirect you back with proper error messages
         $attributes = request()->validate([
             'email' => 'required|email',
             'password' => 'required',
@@ -21,14 +23,19 @@ class SessionsController extends Controller
         // Attempt to authenticate and login the user based on the provided credentials
         // attempt() checks the credentials and also logs the user in
         if (Auth::attempt($attributes)) {
+            request()->session()->regenerate(); // Regenerate the session Id to prevent session fixation
             // Redirect with a success flash message
             return redirect('/')->with('success', 'Welcome Back, ' . auth()->user()->name);
         }
 
         // Incase the validation is successful but the credentials are wrong
-        return back()
-            ->withInput() // Return the old supplied form data so the user doesnot have to re-enter them
-            ->withErrors(['email' => 'Your provided credentials could not be verified']);
+        // return back()
+        //     ->withInput() // Return the old supplied form data so the user doesnot have to re-enter them
+        //     ->withErrors(['email' => 'Your provided credentials could not be verified']);
+
+        throw ValidationException::withMessages([
+            'email' => 'Your provided credentials could not be verified'
+        ]);
     }
 
     public function destroy()
