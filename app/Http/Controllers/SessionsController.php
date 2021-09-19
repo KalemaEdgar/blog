d59d89c640e64ddd9a37104ea6e94353b6b3544d
Kalema Edgar <eked290@gmail.com>
2021-09-19 22:59:45 +0300

    Build the login page

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -5,6 +5,32 @@ use Illuminate\Support\Facades\Auth;
 
 class SessionsController extends Controller
 {
+    public function create()
+    {
+        return view('sessions.create');
+    }
+
+    public function store()
+    {
+        // Validate the request
+        $attributes = request()->validate([
+            'email' => 'required|email',
+            'password' => 'required',
+        ]);
+
+        // Attempt to authenticate and login the user based on the provided credentials
+        // attempt() checks the credentials and also logs the user in
+        if (Auth::attempt($attributes)) {
+            // Redirect with a success flash message
+            return redirect('/')->with('success', 'Welcome Back, ' . auth()->user()->name);
+        }
+
+        // Incase the validation is successful but the credentials are wrong
+        return back()
+            ->withInput() // Return the old supplied form data so the user doesnot have to re-enter them
+            ->withErrors(['email' => 'Your provided credentials could not be verified']);
+    }
+
     public function destroy()
     {
         Auth::logout();
--- routes/web.php
+++ routes/web.php
@@ -6,11 +6,12 @@ use App\Http\Controllers\SessionsController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [PostController::class, 'index'])->name('home');
-
 Route::get('posts/{post:slug}', [PostController::class, 'show']);
 
 Route::get('register', [RegisterController::class, 'create'])->middleware('guest'); // Show the register page. Only a guest can access this page (not logged in)
 
 Route::post('register', [RegisterController::class, 'store'])->middleware('guest'); // Create or register the user to the database
 
-Route::post('logout', [SessionsController::class, 'destroy']);
+Route::get('login', [SessionsController::class, 'create'])->middleware('guest'); // Show the login page or link if you are a guest
+Route::post('login', [SessionsController::class, 'store'])->middleware('guest'); // Login the user
+Route::post('logout', [SessionsController::class, 'destroy'])->middleware('auth'); // Only access the logout link when the user is already signed in
