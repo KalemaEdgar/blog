4b11cb60bb6c2125d2d4cfe23a35437640dea562
Kalema Edgar <eked290@gmail.com>
2021-09-29 00:06:28 +0300

    Mailchimp API tinkering

--- routes/web.php
+++ routes/web.php
@@ -31,6 +31,27 @@ Route::post('login', [SessionsController::class, 'store'])->middleware('guest');
 // Only access the logout link if the user is already signed in using the middleware
 Route::post('logout', [SessionsController::class, 'destroy'])->middleware('auth');
 
+// Mailchimp for newsletters
+Route::get('ping', function () {
+    $mailchimp = new \MailchimpMarketing\ApiClient();
+
+    $mailchimp->setConfig([
+        'apiKey' => config('services.mailchimp.key'),
+        'server' => 'us5'
+    ]);
+
+    // $response = $mailchimp->ping->get(); // Health check for the Mailchimp API
+    // $response = $mailchimp->lists->getAllLists(); // Get all lists
+    // $response = $mailchimp->lists->getList('76607106aa'); // Get my list info
+    // $response = $mailchimp->lists->getListMembersInfo('76607106aa'); // Get my list members
+    $response = $mailchimp->lists->addListMember('76607106aa', [
+        'email_address' => 'laravel@gmail.com',
+        'status' => 'subscribed',
+    ]); // Add a member to a list
+
+    dd($response);
+});
+
 Route::fallback(function () {
     echo 'This is a fallback route incase you need to do something when a route doesnot exist but donot want to use the Laravel default pages which are great.';
 });
